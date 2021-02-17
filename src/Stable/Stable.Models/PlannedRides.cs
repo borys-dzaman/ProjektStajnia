2fab91c49a5f381801ec5aea92895916bd3b16bb
Borys Dzaman <borys.dzaman@gmail.com>
2021-02-17 14:39:09 +0100

    added PlannedRides and Rides models

---
+++ src/Stable/Stable.Models/PlannedRides.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stable.Models
+{
+	public class PlannedRides
+	{
+		public int RiderId { get; }
+		public int RideId { get; }
+		public DateTime RideDate { get; set; }
+		public enum PlannedRideStatus
+        {
+			None,
+			Planned,
+			Completed,
+			CancelRequested,
+			Cancelled
+        }		
+	}
+}
