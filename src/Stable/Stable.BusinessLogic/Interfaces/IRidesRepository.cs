0e6257b69ce9f0beccacc31afe301c6dbf77978b
Borys Dzaman <borys.dzaman@gmail.com>
2021-03-04 21:55:27 +0100

    added IRidesRepository

---
+++ src/Stable/Stable.BusinessLogic/Interfaces/IRidesRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stable.Models
+{
+	public interface IRidesRepository
+	{
+		Ride GetById(int RideId);
+		void Add(Ride ride);
+		void Remove(Ride ride);
+		void Update(Ride ride);
+	}
+}
